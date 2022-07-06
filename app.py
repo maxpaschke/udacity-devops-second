d8ad7d44b37482bbf8c1d3df2ee75846f1c9f47a
Max Paschke <max.paschke@de.bosch.com>
2022-07-06 15:18:19 +0200

    Downgrade to python 3.7.9

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-import joblib
+from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
@@ -56,8 +56,8 @@ def predict():
 
     try:
         clf = joblib.load("boston_housing_prediction.joblib")
-    except:
-        LOG.info("JSON payload: %s json_payload")
+    except Exception as e:
+        LOG.info("JSON payload: %s json_payload " + str(e))
         return "Model not loaded"
 
     json_payload = request.json
